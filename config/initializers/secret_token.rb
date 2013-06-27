3e245d8fb31fc36befc4e48aedba0b344982bb40
Philip Hale <p.hale.09@aberdeen.ac.uk>
2013-06-27 21:18:46 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Budgeter::Application.config.secret_key_base = 'e091e850785c55ff9bfad62e11e83a4244824b22c2c4cf52865db692a8b67ae9ea8c936e911af9fd862be014fc49dd57556301237fd1c62323dd08efbc7a68bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Budgeter::Application.config.session_store :cookie_store, key: '_budgeter_session'
